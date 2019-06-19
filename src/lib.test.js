8c9a8917f1ca65e96a82e032a042cec072352b1e
Cam-del <46486074+Cam-del@users.noreply.github.com>
2019-06-19 20:50:41 +0100

    Update lib.test.js

--- src/lib.test.js
+++ src/lib.test.js
@@ -1,5 +1,5 @@
 const lib = require('./lib');
 
 test('helloWorld() returns some special text', () => {
-  expect(lib.helloWorld()).toBe('Hello World! I have been deployed in Kubernetes!');
+  expect(lib.helloWorld()).toBe('Hello World! I have been deployed in Kubernetes! With great difficulty and tears flowing down my face');
 });
