8dbe6f9f7c5d625729a47779725d8782f2aabda0
Cam-del <46486074+Cam-del@users.noreply.github.com>
2019-06-19 20:49:36 +0100

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,4 @@
-const helloWorld = () => 'Hello World! I have been deployed in Kubernetes!'
+const helloWorld = () => 'Hello World! I have been deployed in Kubernetes! With great difficulty and tears flowing down my face'
 
 module.exports = {
     helloWorld
